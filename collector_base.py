b1048ad26258de71202059997f7c201ce37d875e
Karl W Schulz <karl.schulz@amd.com>
2023-08-02 12:21:33 -0500

    convert Collector  to abstract base class

--- collector_base.py
+++ collector_base.py
@@ -1,6 +1,6 @@
 # Prometheus data collector for HPC systems.
 #
-# Base collector class - defines required methods for all metric collectors implemented as a child class.
+# Base Collector class - defines required methods for all metric collectors implemented as a child class.
 #--
 
 import logging
@@ -15,45 +15,3 @@ class Collector(ABC):
     @abstractmethod
     def updateMetrics(self):
         pass
-
-# class Metrics(ABC):
-#     def __init__(self):
-#         logging.basicConfig(
-#             format="%(message)s", level=logging.DEBUG, stream=sys.stdout
-#         )
-#         # defined GPU prometheus metrics (stored on a per-gpu basis)
-#         self.__GPUmetrics = {}
- 
-#         # defined global prometheus metrics
-#         self.__globalMetrics = {}
-#         self.__registry_global = CollectorRegistry()
-#         # Resource manager type
-#         self.__rms = "slurm"
-
-#         # define desired collectors
-#         self.__collectors = []
-
-#         rocmSMI = True
-#         if rocmSMI:
-#             from collectors import ROCM
-#             self.__collectors.append(ROCM())
-
-#         logging.debug("Completed collector initialization (base class)")
-#         return
-
-    # # Required method to implemented by child classes
-    # def registerMetrics(self):
-    #     logging.error("[ERROR]: data collectors must implement registerMetrics() method.")
-    #     sys.exit(1)
-    #     return
-
-    # # Required method to implemented by child classes
-    # @abstractmethod
-    # def registerMetrics(self):
-    #     pass
-
-    # # Required method to implemented by child classes
-    # def updateMetrics(self):
-    #     logging.error("[ERROR]: data collectors must implement updateMetrics() method.")
-    #     sys.exit(1)
-    #     return
