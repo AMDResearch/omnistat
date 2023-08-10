1918533e1bae6812ca4283fa4e97e6a647cdc0c0
Karl W Schulz <karl.schulz@amd.com>
2023-08-10 12:56:28 -0500

    allow for ROCM_SMI_PATH env variable to specify path to desired rocm-smi binary. Cache the rocm-smi query command.

--- collectors.py
+++ collectors.py
@@ -8,17 +8,39 @@ import sys
 import utils
 import json
 import logging
+import os
+import shutil
 from collector_base import Collector
 from prometheus_client import Gauge, generate_latest, CollectorRegistry
 
 class ROCMSMI(Collector):
     def __init__(self):
-        logging.debug("Initializing ROCm data collector")
+        logging.debug("Initializing ROCm SMI data collector")
         prefix = "rocm_"
         
-        # command-line flags for use with rocm-smi to obtained desired metrics
-        self.__rocm_smi_flags = "-P -u -f -t --showmeminfo vram --json"
+        # allow for env variable to prescribe path to rocm-smi
+        rocm_smi_command = "rocm-smi"
+        if "ROCM_SMI_PATH" in os.environ:
+            rocmPath = os.getenv("ROCM_SMI_PATH")
+            logging.debug("Overriding path with ROCM_SMI_PATH=%s" % rocmPath)
+            if os.path.isdir(rocmPath):
+                rocm_smi_command = rocmPath + "/rocm-smi"
+            else:
+                utils.error("provided ROCM_SMI_PATH does not exist -> %s" % rocmPath)
+                sys.exit(1)
+        
+        # verify we can resolve the rocm-smi binary
+        path = shutil.which(rocm_smi_command)
+        if not path:
+            utils.error("Unable to resolve path for rocm-smi -> %s" % rocm_smi_command)
+        else:
+            logging.debug("rocm-smi path = %s" % path)
 
+        # command-line flags for use with rocm-smi to obtained desired metrics
+        rocm_smi_flags = "-P -u -f -t --showmeminfo vram --json"
+        # cache query command with options
+        self.__rocm_smi_query = [rocm_smi_command] + rocm_smi_flags.split()
+ 
         # list of desired metrics to query: (prometheus_metric_name -> rocm-smi-key)
         self.__rocm_smi_metrics = {
             prefix+"temp_die_edge": "Temperature (Sensor edge) (C)",
@@ -27,7 +49,8 @@ class ROCMSMI(Collector):
             prefix+"vram_total": "VRAM Total Memory (B)",
             prefix+"vram_used": "VRAM Total Used Memory (B)",
         }
-        logging.debug("rocm_smi_flags = %s" % self.__rocm_smi_flags)
+
+        logging.debug("rocm_smi_exec = %s" % self.__rocm_smi_query)
 
         self.__GPUmetrics = {}
 
@@ -37,8 +60,7 @@ class ROCMSMI(Collector):
     def registerMetrics(self):
         """Run rocm-smi and register metrics of interest"""
 
-        command = ["rocm-smi"] + self.__rocm_smi_flags.split()
-        data = utils.runShellCommand(command, exit_on_error=True)
+        data = utils.runShellCommand(self.__rocm_smi_query, exit_on_error=True)
         data = json.loads(data.stdout)
         for gpu in data:
             logging.debug("%s: gpu detected" % gpu)
@@ -87,8 +109,7 @@ class ROCMSMI(Collector):
         # ---
         # Collect and parse latest GPU metrics from rocm-smi
         # ---
-        command = ["rocm-smi"] + self.__rocm_smi_flags.split()
-        data = utils.runShellCommand(command)
+        data = utils.runShellCommand(self.__rocm_smi_query)
         try:
             data = json.loads(data.stdout)
         except:
