a39f900b0f67bcb49417a0bcf1900b23cbb2b8b8
Karl W Schulz <karl.schulz@amd.com>
2023-08-10 08:22:43 -0500

    cleanup

--- collector_base.py
+++ collector_base.py
@@ -1,17 +1,21 @@
 # Prometheus data collector for HPC systems.
 #
-# Base Collector class - defines required methods for all metric collectors implemented as a child class.
+# Base Collector class - defines required methods for all metric collectors 
+# implemented as a child class.
 #--
 
-import logging
-import sys
 from abc import ABC, abstractmethod
 
 class Collector(ABC):
     # Required methods to be implemented by child classes
     @abstractmethod
     def registerMetrics(self):
+        """Defines desired metrics to monitor with Prometheus. Called once during initialization.
+        """
         pass
+
     @abstractmethod
     def updateMetrics(self):
+        """Updates defined metrics with latest values. Called at every polling interval.
+        """
         pass
