3e1a60aaddfccdc36fb2b6dd39b9afcbde3fe795
Karl W Schulz <karl.schulz@amd.com>
2023-08-14 14:43:55 -0500

    add a resolvePath() convenience function

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 import logging
 import subprocess
 import sys
+import os
+import shutil
+
+def error(message):
+    """Log an error message and exit
+
+    Args:
+        message (string): message
+    """
+    logging.error("Error: " + message)
+    sys.exit(1)
 
 def runShellCommand(command, capture_output=True, text=True, exit_on_error=False):
     """Runs a provided shell command
@@ -22,6 +33,32 @@ def runShellCommand(command, capture_output=True, text=True, exit_on_error=False
         sys.exit(1)
     return results
 
-def error(message):
-    logging.error("Error: " + message)
-    sys.exit(1)
+def resolvePath(desiredCommand,envVar):
+    """Resolve underlying path to a desired shell command.
+
+    Args:
+        desiredCommand (string): desired shell command to resolve
+        envVar (string): environment variable string that can optionally provide path to desired command
+
+    Returns:
+        string: resolved path to desired comman
+    """
+    command = desiredCommand
+    if envVar in os.environ:
+        customPath = os.getenv(envVar)
+        logging.debug("Overriding command search path with %s=%s" % (envVar,customPath))
+        if os.path.isdir(customPath):
+            command = customPath + "/" + desiredCommand
+        else:
+            error("provided %s does not exist -> %s" % (envVar,customPath))
+            sys.exit(1)
+
+    # verify we can resolve the desired binary
+    path = shutil.which(command)
+    if not path:
+        error("Unable to resolve path for %s" % command)
+    else:
+        logging.debug("--> %s path = %s" % (desiredCommand,path))
+
+    return path
+
