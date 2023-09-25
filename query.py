f9cde62ed811a35e0e8eac6b63a718cf213422c2
Karl W Schulz <karl.schulz@amd.com>
2023-09-25 15:31:44 -0500

    updated to work with newer exporter approach

--- query.py
+++ query.py
@@ -3,6 +3,28 @@ from prometheus_api_client.utils import parse_datetime
 from datetime import datetime, timedelta
 import sys
 import numpy as np
+import subprocess
+
+# define local site configuration
+config= {}
+config["mi1004x"]     = {"num_gpus":4}
+config["mi1008x"]     = {"num_gpus":8}
+config["system_name"] = "HPC Fund"
+
+def query_slurm_job(id):
+    cmd = ["sacct","-n","-P","-X","-j",id,"--format","Start,End,NNodes,Partition"]
+    results = subprocess.check_output(cmd,universal_newlines=True).strip()
+    results = results.split("\n")
+    assert(len(results)==1)
+
+    jobdata = {}
+    data = results[0].split("|")
+    jobdata["begin_date"] = data[0]
+    jobdata["end_date"]   = data[1]
+    jobdata["num_nodes"]  = data[2]
+    jobdata["partition"]  = data[3]
+
+    return jobdata
 
 #prometheus_url = "http://10.0.100.11:9090"
 prometheus = PrometheusConnect(url="http://10.0.100.11:9090")
@@ -11,61 +33,91 @@ prometheus = PrometheusConnect(url="http://10.0.100.11:9090")
 #query = 'up == 1'
 jobID=11973
 query = 'slurmjob_jobid==11962'
-query = 'slurmjob_jobid==11973'
+query = 'slurmjob_info{jobid="12263"}'
 #query = 'slurmjob_jobid'
 
-# Query Prometheus for the time series data
-#results = prometheus.custom_query(query=query)
-#results = prometheus.custom_query(query=query, params={'step':'5m'})
-#results = prometheus.custom_query(query=query, params={'time': '5m'})
+jobid = "12263"
+jobid = "12271"
+jobid = "12304"
+jobid = "12318"
+jobinfo = query_slurm_job(jobid)
 
-now = datetime.now().replace(minute=0,second=0,microsecond=0)
-start_time = now - timedelta(days=4)
+# now = datetime.now().replace(minute=0,second=0,microsecond=0)
+# start_time = now - timedelta(days=4)
 #now = datetime.now().strftime('%Y-%m-%d %H:%M:00')
 # formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
 
-start_time = datetime.strptime("2023-09-11T16:57:52",'%Y-%m-%dT%H:%M:%S')
-end_time   = datetime.strptime("2023-09-11T17:27:46",'%Y-%m-%dT%H:%M:%S')
+# start_time = datetime.strptime("2023-09-11T16:57:52",'%Y-%m-%dT%H:%M:%S')
+# end_time   = datetime.strptime("2023-09-11T17:27:46",'%Y-%m-%dT%H:%M:%S')
 
-start_time = datetime.strptime("2023-09-11T17:51:05",'%Y-%m-%dT%H:%M:%S')
-end_time   = datetime.strptime("2023-09-12T17:51:31",'%Y-%m-%dT%H:%M:%S')
+# start_time = datetime.strptime("2023-09-11T17:51:05",'%Y-%m-%dT%H:%M:%S')
+# end_time   = datetime.strptime("2023-09-12T17:51:31",'%Y-%m-%dT%H:%M:%S')
+
+
+start_time = datetime.strptime("2023-09-15T17:51:05",'%Y-%m-%dT%H:%M:%S')
+end_time   = datetime.strptime("2023-09-20T17:51:31",'%Y-%m-%dT%H:%M:%S')
+
+
+
+
+start_time = datetime.strptime(jobinfo['begin_date'],'%Y-%m-%dT%H:%M:%S')
+if jobinfo['end_date'] == "Unknown":
+    end_time   = datetime.now()
+else:   
+    end_time   = datetime.strptime(jobinfo['end_date'],  '%Y-%m-%dT%H:%M:%S')
+
+## print("job = %s" % jobid)
+## print("--> start time = %s" % start_time)
+## print("--> end   time = %s" % end_time)
+#sys.exit(1)
 
 # print(now)
 # print(start_time)
 # print(end_time)
-#print(formatted_time)
-#sys.exit(1)
+# sys.exit(1)
 
-results = prometheus.custom_query_range(query=query,start_time=start_time,end_time = end_time,step=60)
+# print(query)
+# results = prometheus.custom_query_range(query=query,start_time=start_time,end_time = end_time,step=60)
+#results = prometheus.custom_query(query=query)
+#results = prometheus.custom_query_range('card0_rocm_utilization * on (instance) slurmjob_info{jobid="%s"}',start_time,end_time,step=60)
+results = prometheus.custom_query_range("card0_rocm_utilization * on (instance) slurmjob_info{jobid=\"%s\"}" % jobid,start_time,end_time,step=60)
+# #results = prometheus.custom_query("card0_rocm_utilization")
+# print(results)
+# sys.exit(1)
 
 # Detect hosts associated with this job
 hosts = []
 for result in results:
     hosts.append(result['metric']['instance'])
 
-# print("# of active hosts = %i" % len(hosts) )
-# print(hosts)
+print("# of active hosts = %i" % len(hosts) )
+print(hosts)
+#
 
 statistics = {}
+numGpus = config[jobinfo['partition']]['num_gpus']
+print(numGpus)
 
 # Query metrics from assigned hosts during the job begin/end start period
-for metric in ["card0_rocm_utilization","card1_rocm_utilization","card2_rocm_utilization","card3_rocm_utilization",
-                "card4_rocm_utilization","card5_rocm_utilization","card6_rocm_utilization","card7_rocm_utilization"]:
+#for metric in ["card0_rocm_utilization","card1_rocm_utilization","card2_rocm_utilization","card3_rocm_utilization",
+#                "card4_rocm_utilization","card5_rocm_utilization","card6_rocm_utilization","card7_rocm_utilization"]:
+for gpu in range(numGpus):
+    metric = "card" + str(gpu) + "_rocm_utilization"
+
     for host in hosts:
-        query='avg(card0_rocm_utilization{instance="t004-008:8000"})'
-        #query=metric + "{instance=\"" + host + "\"}"
-        #query_avg = avg
-        #print(query)
-        #results = prometheus.custom_query_range(query=query,start_time=start_time,end_time = end_time,step=60)
-        results= prometheus.get_metric_range_data(metric,start_time=start_time,end_time=end_time,
-        label_config={'instance':'t004-008:8000'})
-        # df = MetricSnapshotDataFrame(results)
-        # print(results)
-        # print(len(results))
-        #print(results)
-        #print(len(results))
+        # print(metric)
+        results = prometheus.custom_query_range("avg(%s * on (instance) slurmjob_info{jobid=\"%s\"})" % (metric,jobid),start_time,end_time,step=60)
+
+        # if metric == "card1_rocm_utilization":
+        #     mydata = results[0]['values']
+        #     for item in mydata:
+        #         print("%s %s" % (datetime.fromtimestamp(item[0]),item[1]))
+        #     sys.exit(1)
+
         assert(len(results) == 1)
         data = results[0]['values']
+        # print(data)
+        # sys.exit(1)
         # print("%s %s %s (%i)" % (host,datetime.fromtimestamp(data[0][0]),datetime.fromtimestamp(data[-1][0]),
         #     len(data)))
 
@@ -86,8 +138,6 @@ for metric in ["card0_rocm_utilization","card1_rocm_utilization","card2_rocm_uti
 
 # summarize statistics
 
-numGpus = 8
-# numNodes = 1
 system = "HPC Fund"
 
 print(" ")
@@ -99,35 +149,7 @@ print(" --> Start time = %s" % start_time)
 print(" --> End   time = %s" % end_time)
 print("")
 print("--> GPU Core Utilization")
-print("   | GPU # | Min (%) | Max (%) | Mean (%) |")
+print("   | GPU # | Min (%) | Max (%) | Mean (%)|")
 for card in range(numGpus):
     key = "card" + str(card) + "_rocm_utilization"
     print("   |%6s |%7.1f  |%7.1f  |%7.1f  |" % (card,statistics[key+"_min"],statistics[key+"_max"],statistics[key+"_mean"]))
-
-
-
-# # Find min/max times and instance names
-# for result in results:
-#     # metric_name = result['metric']['__name__']
-#     # instance = result['metric'].get('instance', '')
-#     # value = result['value'][1]
-
-#     # print(f"Metric: {metric_name}, Instance: {instance}, Value: {value}")
-#     # print(result['metric'])
-#     # print(type(result))
-#     # print(type(result['values']))
-# #    print(result)
-#     # sys.exit(1)
-#     minTime = result['values'][0][0]
-#     maxTime = result['values'][-1][0]
-#     numPoints = len(result['values'])
-# #    print(numPoints)
-#     print("Instance = %s Min time = %s Max time = %s (%s points)" % (result['metric']['instance'],
-#             datetime.fromtimestamp(minTime),datetime.fromtimestamp(maxTime),numPoints))
-# #    for value in result['values']:
-# #        print(value[0],value[1])
-#         # print(datetime.fromtimestamp(value[0]),value[1])
-# #    sys.exit(1)
-
-# print(results[0])
-# print(results[1])
