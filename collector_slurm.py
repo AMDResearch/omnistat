60838b78a03a7f6f834c6ad9344a000afa821867
Karl W Schulz <karl.schulz@amd.com>
2023-12-04 14:46:42 -0600

    update for slurm collector; account for possibility of running in user-mode where the collector is fired up within the auspices of a SLURM job; in that case, cache the relevant job info at startup and do not use squeue; help on production systems where slurm can be slow to respond.

--- collector_slurm.py
+++ collector_slurm.py
@@ -39,26 +39,52 @@ import platform
 from collector_base import Collector
 from prometheus_client import Gauge, generate_latest, CollectorRegistry
 
+
 class SlurmJob(Collector):
     def __init__(self):
         logging.debug("Initializing SlurmJob data collector")
         self.__prefix = "slurmjob_"
-
-        # setup slurm binary path
-        command = utils.resolvePath("squeue", "SLURM_PATH")
-        # command-line flags for use with squeue to obtained desired metrics
-        hostname = platform.node().split(".", 1)[0]
-        flags = (
-            "-w "
-            + hostname
-            + " -h  --Format=JobID::,UserName::,Partition::,NumNodes::,BatchFlag"
-        )
-        # cache query command with options
-        self.__squeue_query = [command] + flags.split()
-
-        logging.debug("sqeueue_exec = %s" % self.__squeue_query)
-
+        self.__userMode = False
         self.__SLURMmetrics = {}
+        self.__slurmJobInfo = []
+
+        # check if running within slurm job (user mode profiling) - if so, cache jobinfo
+        if "SLURM_JOB_ID" in os.environ:
+            self.__userMode = True
+            logging.info("Running slurm collector within slurm job...")
+            jobinfo = [None] * 5
+
+            try:
+                jobinfo[0] = os.environ["SLURM_JOB_ID"]
+                jobinfo[1] = os.environ["SLURM_JOB_USER"]
+                jobinfo[2] = os.environ["SLURM_JOB_PARTITION"]
+                jobinfo[3] = os.environ["SLURM_NNODES"]
+            except:
+                utils.error(
+                    "Unable to query expected SLURM environment variables in user mode"
+                )
+
+            if "SLURM_LAUNCH_NODE_IPADDR" in os.environ:
+                jobinfo[4] = 0  # interactive job
+            else:
+                jobinfo[4] = 1  # batch job
+
+            self.__slurmJobInfo = jobinfo
+
+        else:
+            # setup squeue binary path to query slurm to determine node ownership
+            command = utils.resolvePath("squeue", "SLURM_PATH")
+            # command-line flags for use with squeue to obtained desired metrics
+            hostname = platform.node().split(".", 1)[0]
+            flags = (
+                "-w "
+                + hostname
+                + " -h  --Format=JobID::,UserName::,Partition::,NumNodes::,BatchFlag"
+            )
+            # cache query command with options
+            self.__squeue_query = [command] + flags.split()
+
+            logging.debug("sqeueue_exec = %s" % self.__squeue_query)
 
     def registerMetrics(self):
         """Register metrics of interest"""
@@ -79,18 +105,23 @@ class SlurmJob(Collector):
             logging.debug("--> Registered SLURM metric = %s" % metric)
 
     def updateMetrics(self):
-        data = utils.runShellCommand(self.__squeue_query)
-
         self.__SLURMmetrics["info"].clear()
         self.__SLURMmetrics["annotations"].clear()
+        jobEnabled = False
 
-        # Case when SLURM job is allocated
-        if data.stdout.strip():
+        if self.__userMode == True:
+            results = self.__slurmJobInfo
+            jobEnabled = True
+        else:
+            data = utils.runShellCommand(self.__squeue_query)
             # query output format:
             # JOBID,USER,PARTITION,NODES,CPUS
-            results = data.stdout.strip().split(":")
+            if data.stdout.strip():
+                results = data.stdout.strip().split(":")
+                jobEnabled = True
 
-            self.__SLURMmetrics["info"].clear()
+        # Case when SLURM job is allocated
+        if jobEnabled:
             self.__SLURMmetrics["info"].labels(
                 jobid=results[0],
                 user=results[1],
