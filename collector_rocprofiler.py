01827a58372ab7b81990ba7141eb73fdb99a59fa
Karl W Schulz <karl.schulz@amd.com>
2024-04-10 14:46:45 -0500

    slight rocprofiler collector refactor: tweak python side to handle metric updates for multiple GPUs
    Signed-off-by: Karl W Schulz <karl.schulz@amd.com>

--- collector_rocprofiler.py
+++ collector_rocprofiler.py
@@ -12,22 +12,26 @@ class rocprofiler(Collector):
         logging.debug("Initializing rocprofiler data collector")
         self.__names = names
         self.__session = DeviceSession()
-        num_gpus = self.__session.create(self.__names)
-        self.__metrics = [[]] * num_gpus
-        logging.info("pyrocprofiler initialized")
+        self.__num_gpus = self.__session.create(self.__names)
+        logging.info(
+            "--> pyrocprofiler: number of GPUs detected = %i" % self.__num_gpus
+        )
+        self.__GPUmetrics = [None] * self.__num_gpus
+        logging.info("--> pyrocprofiler initialized")
 
     def registerMetrics(self):
-        for gpu_id, gpu_metrics in enumerate(self.__metrics):
-            prefix = f"card{gpu_id}_rocprofiler_"
-            for name in self.__names:
-                metric_name = prefix + name
-                gpu_metrics.append(Gauge(metric_name, ""))
+        for gpu in range(self.__num_gpus):
+            self.__GPUmetrics[gpu] = []
+            prefix = f"card{gpu}_rocprofiler_"
+            for metric in self.__names:
+                metric_name = prefix + metric
+                self.__GPUmetrics[gpu].append(Gauge(metric_name, ""))
                 logging.info("  --> [registered] %s (gauge)" % (metric_name))
         self.__session.start()
 
     def updateMetrics(self):
         values = self.__session.poll()
-        for i, _ in enumerate(self.__metrics):
-            for j, _ in enumerate(self.__names):
-                self.__metrics[i][j].set(values[i][j])
+        for gpu in range(self.__num_gpus):
+            for j, _ in enumerate(values[gpu]):
+                self.__GPUmetrics[gpu][j].set(values[gpu][j])
         return generate_latest()
