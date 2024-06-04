438c5a048366f7d83fa53a51783717670b215022
Jordà Polo <jorda.polo@amd.com>
2024-06-04 18:55:20 -0500

    Include and load default configuration in package

--- omniwatch/monitor.py
+++ omniwatch/monitor.py
@@ -29,6 +29,7 @@
 #--
 
 import configparser
+import importlib
 import logging
 import os
 import platform
@@ -47,11 +48,20 @@ class Monitor():
             format="%(message)s", level=logging.INFO, stream=sys.stdout
         )
 
-        # setup runtine config that can be overridden via env variable
-        configFile = "omniwatch.default"
-        if "OMNIWATCH_CONFIG" in os.environ:
-            logging.info("Overriding default config file")
-            configFile = os.environ["OMNIWATCH_CONFIG"]
+        # Resolve path to default config file in the current installation.
+        # This configuration is only meant to provide sane defaults to run
+        # locally, but most installations will need a custom file.
+        # It can be overridden using the configFile option below.
+        packageDir = importlib.resources.files("omniwatch")
+        defaultConfigFile = packageDir.joinpath("config/omniwatch.default")
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--configFile",type=str,
+                            help=f"runtime config file (default={defaultConfigFile})",
+                            default=defaultConfigFile)
+        args = parser.parse_args()
+
+        configFile = args.configFile
 
         self.runtimeConfig = {}
 
@@ -89,7 +99,7 @@ class Monitor():
         # define desired collectors
         self.__collectors = []
 
-         # allow for disablement of slurm collector via regex match
+        # allow for disablement of slurm collector via regex match
         if self.runtimeConfig['collector_enable_slurm']:
             if config.has_option('omniwatch.collectors.slurm','host_skip'):
                 host_skip = utils.removeQuotes(config['omniwatch.collectors.slurm']['host_skip'])
