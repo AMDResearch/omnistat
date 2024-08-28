984173344faf6481078ca95aedd26bea79f00f3e
Jordà Polo <jorda.polo@amd.com>
2024-08-28 08:51:02 -0700

    Split test configuration to support multiple test files

---
+++ test/config.py
@@ -0,0 +1,15 @@
+import shutil
+
+# Variable used to skip tests that depend on a ROCm installation; assume
+# ROCm is installed if we can find `rocminfo' in the host.
+rocm_host = True if shutil.which("rocminfo") else False
+
+# List of available nodes in the test environment.
+nodes = ["node1", "node2"]
+
+# Prometheus URL and query configuration.
+prometheus_url = "http://localhost:9090/"
+time_range = "30m"
+
+# Omnistat monitor port; same port is used for system and user tests.
+port = "8000"
