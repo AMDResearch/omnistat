fee4a2e5fec4619a820a10b97713c1d18b57e043
Omri Avraham <oavraham@amd.com>
2024-09-30 15:44:15 -0600

    refactor code, collector needs to run on each compute node

--- omnistat/collector_rms_v2.py
+++ omnistat/collector_rms_v2.py
@@ -31,11 +31,11 @@ user-provided annotation timestamps.
 """
 
 import logging
-
+import platform
+import re
 from prometheus_client import Gauge
-
+from omnistat import utils
 from omnistat.collector_base import Collector
-from omnistat.job_metrics_collector import get_job_info
 
 
 class RMSJobV2(Collector):
@@ -48,12 +48,24 @@ class RMSJobV2(Collector):
         self.__lastAnnotationLabel = None
         self.c = 0
 
+        # setup squeue binary path to query slurm to determine node ownership
+        command = utils.resolvePath("squeue", "SLURM_PATH")
+        # command-line flags for use with squeue to obtained desired metrics
+        self.hostname = platform.node().split(".", 1)[0]
+        flags = "-w " + self.hostname + " -h  --Format=JobID::,UserName::,Partition::,NumNodes::,BatchFlag"
+        # cache query command with options
+        self.__squeue_query = [command] + flags.split()
+        # job step query command
+        flags = "-s -w " + self.hostname + " -h --Format=StepID"
+        self.__squeue_steps = [command] + flags.split()
+        logging.debug("sqeueue_exec = %s" % self.__squeue_query)
+
     def registerMetrics(self):
         """Register metrics of interest"""
 
         # alternate approach - define an info metric
         # (https://ypereirareis.github.io/blog/2020/02/21/how-to-join-prometheus-metrics-by-label-with-promql/)
-        labels = ["jobid", "start_time", "end_time", "submit_time", "node", "card", "user", "account"]
+        labels = ["jobid", "user", "partition", "nodes", "batchflag", "jobstep", "type", "account", "card"]
         self.__RMSMetrics["info"] = Gauge(self.__prefix + "info", "RMS job details", labels)
 
         for metric in self.__RMSMetrics:
@@ -67,42 +79,165 @@ class RMSJobV2(Collector):
             if counter < self.c:
                 # catch edge case for multiple metrics with same name
                 try:
-                    self.__RMSMetrics["info"].remove(metric[0], metric[1], metric[2], metric[3], metric[4], metric[5], metric[6], metric[7])
-                except:
+                    self.__RMSMetrics["info"].remove(metric[0], metric[1], metric[2], metric[3], metric[4], metric[5], metric[6], metric[7], metric[8])
+                except Exception as e:
+                    logging.error(f"Failed to remove metric: {metric}\n"
+                                  f"error: {e}\n"
+                                  f"Running Jobs: {self.__RunningJobs}\n"
+                                  f"Metrics: {self.__RMSMetrics}")
                     pass
 
         return
 
+    def querySlurmJob(self, timeout=1, exit_on_error=False, mode="squeue"):
+        """
+        Query SLURM and return job info for local host.
+        Supports two query modes: squeue call and read from file.
+
+        Returns dictionary containing job id, user, partition, # of nodes, and batchmode flag
+        """
+        keys = [
+            "RMS_JOB_ID",
+            "RMS_JOB_USER",
+            "RMS_JOB_PARTITION",
+            "RMS_JOB_NUM_NODES",
+            "RMS_JOB_BATCHMODE",
+        ]
+        results = []
+
+        if mode == "squeue":
+            squeue_data = utils.runShellCommand(self.__squeue_query, timeout=timeout, exit_on_error=exit_on_error)
+            # squeue query output format: JOBID:USER:PARTITION:NUM_NODES:BATCHFLAG
+            squeue_data_out = squeue_data.stdout.strip()
+            if squeue_data_out:
+                for job in squeue_data_out.splitlines():
+                    data = job.strip().split(":")
+                    r_job = dict(zip(keys, data))
+                    r_job["RMS_TYPE"] = "slurm"
+
+                    # require a 2nd query to ascertain job steps (otherwise, miss out on batchflag)
+                    squeue_step_data = utils.runShellCommand(self.__squeue_steps, timeout=timeout, exit_on_error=exit_on_error)
+                    r_job["RMS_STEP_ID"] = -1
+                    if squeue_step_data.stdout.strip():
+                        # If we are in an active job step, the STEPID will have an integer index appended, e.g.
+                        # 57735.10
+                        # 57735.interactive
+                        stepField = (squeue_step_data.stdout.splitlines()[0]).strip()
+                        jobstep = stepField.split(".")[1]
+                        if jobstep.isdigit():
+                            r_job["RMS_STEP_ID"] = jobstep
+                    results.append(r_job)
+
+        return results
+
+
+    def get_job_info(self, job_id):
+
+        def expand_gpu(input_str):
+            indexes = input_str.split("(IDX:")[-1].split(")")[0]
+            if not indexes:
+                return [""]
+            temp = [(lambda sub: range(sub[0], sub[-1] + 1))(list(map(int, ele.split('-')))) for ele in
+                    indexes.split(',')]
+            res = [b for a in temp for b in a]
+            return res
+
+        def expand_number_range(input_str):
+            if not isinstance(input_str, str):
+                return [input_str]
+            # Match the pattern with numbers and ranges inside square brackets
+            match = re.search(r'(.+)\[(.+)\]', input_str)
+            if match:
+                prefix = match.group(1)
+                ranges = match.group(2).split(',')
+                expanded_list = []
+
+                for item in ranges:
+                    if '-' in item:
+                        start, end = map(int, item.split('-'))
+                        width = len(item.split('-')[0])  # Preserve leading zeros
+                        expanded_list.extend([f"{prefix}{i:0{width}}" for i in range(start, end + 1)])
+                    else:
+                        expanded_list.append(f"{prefix}{item}")
+
+                return expanded_list
+            else:
+                return [input_str]
+
+        result = {
+            "gpus": [],
+            "account": "",
+        }
+        scontrol_data = utils.runShellCommand(['scontrol', 'show', 'job', job_id.strip(), '-d'],
+                                              capture_output=True, text=True)
+        if not scontrol_data.stdout.strip():
+            return result
+        for line in scontrol_data.stdout.strip().splitlines():
+            if "Account=" in line:
+                result["account"] = line.split("Account=")[-1].split(" ")[0].replace("(", "").replace(")", "")
+            if line.strip().startswith("Nodes="):
+                # logging.error(f"Line: {line}")
+                nodes = line.split("Nodes=")[-1].split(" ")[0].strip()
+                # logging.error(f"Nodes: {nodes}")
+                nodes = expand_number_range(nodes)
+                if self.hostname not in nodes:
+                    logging.error(f"Node {self.hostname} not in nodes {nodes}")
+                    continue
+                # logging.error(f"Nodes expanded: {nodes}")
+                gpus = line.split("GRES=")[-1].strip()
+                # logging.error(f"GPUs: {gpus}")
+                gpus = expand_gpu(gpus)
+                result["gpus"] = gpus
+                # logging.error(f"GPUs expanded: {gpus}")
+        return result
+
+
     def collect_data_incremental(self):
         self.c += 1
-        jobs = get_job_info()
+        jobs = self.querySlurmJob()
 
+        keys = [
+            "RMS_JOB_ID",
+            "RMS_JOB_USER",
+            "RMS_JOB_PARTITION",
+            "RMS_JOB_NUM_NODES",
+            "RMS_JOB_BATCHMODE",
+            "RMS_STEP_ID",
+            "RMS_TYPE"
+        ]
+        labels = ["jobid", "user", "partition", "nodes", "batchflag", "jobstep", "type", "account", "card"]
         for job in jobs:
-            jobid = job.get("job_id")
-            start_time = job.get("start_time")
-            end_time = job.get("end_time")
-            submit_time = job.get("submit_time")
-            gpus = job.get("GPUs")
-            user = job.get("user_name")
-            account = job.get("account")
+            jobid = job.get("RMS_JOB_ID")
+            user = job.get("RMS_JOB_USER")
+            partition = job.get("RMS_JOB_PARTITION")
+            num_nodes = job.get("RMS_JOB_NUM_NODES")
+            batchmode = job.get("RMS_JOB_BATCHMODE")
+            stepid = job.get("RMS_STEP_ID")
+            jobtype = job.get("RMS_TYPE")
+            try:
+                results = self.get_job_info(jobid)
+                account = results["account"]
+                gpus = results["gpus"]
+            except Exception as e:
+                account = ""
+                gpus = [""]
+                logging.error(f"Failed to get job info for job: {jobid}\nError: {e}")
 
             if not gpus:
                 gpus = [""]
             for gpu in gpus:
-                # add domain suffix
-                node = gpu.split(":")[0] + ".amd.com"  # temp hack
-                gpu = gpu.split(":")[-1]
-                metric_tuple = (jobid, start_time, end_time, submit_time, node, gpu, user, account)
+                metric_tuple = (jobid, user, partition, num_nodes, batchmode, stepid, jobtype, account, gpu)
                 self.__RunningJobs[metric_tuple] = self.c
                 self.__RMSMetrics["info"].labels(
                     jobid=jobid,
-                    start_time=start_time,
-                    end_time=end_time,
-                    submit_time=submit_time,
-                    node=node,
-                    card=gpu,
                     user=user,
+                    partition=partition,
+                    nodes=num_nodes,
+                    batchflag=batchmode,
+                    jobstep=stepid,
+                    type=jobtype,
                     account=account,
+                    card=gpu
                 ).set(1)
 
         return
