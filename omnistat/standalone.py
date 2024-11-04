d4c4a47556669dfcc2ddd65ed56c47446e95f74a
Karl W Schulz <karl.schulz@amd.com>
2024-11-04 12:46:09 -0600

    refactor standalone collector to periodcially push data to victoriametrics during a job; threading added to support flask endpoint that can be used to terminate the data collector and push final data (previous file-based termination removed).
    Signed-off-by: Karl W Schulz <karl.schulz@amd.com>

--- omnistat/standalone.py
+++ omnistat/standalone.py
@@ -25,18 +25,22 @@
 #
 # Standalone data collector for HPC systems.
 # --> intended for user-mode data collection to run within a job
+# --> provides a flask endpoint to terminate data collection (http://host:port/shutdown)
 
 import argparse
 import ctypes
 import getpass
 import logging
 import os
-import pandas as pd
 import platform
-import sqlite3
+import requests
 import sys
+import signal
+import threading
 import time
 import warnings
+from datetime import datetime, timezone
+from flask import Flask, request, jsonify
 from prometheus_client import Gauge, REGISTRY
 
 # Ensure current directory is part of Python's path; allows direct execution
@@ -47,8 +51,10 @@ if os.path.isdir("omnistat") and sys.path[0]:
 from omnistat import utils
 from omnistat.monitor import Monitor
 
-import requests
-import time
+
+app = Flask(__name__)
+terminateFlagEvent = threading.Event()
+dataDeliveredEvent = threading.Event()
 
 
 def push_to_victoria_metrics(metrics_data_list, victoria_url):
@@ -67,23 +73,19 @@ def push_to_victoria_metrics(metrics_data_list, victoria_url):
 
 
 class Standalone:
-    def __init__(self, args):
-        logging.basicConfig(format="%(message)s", level=logging.ERROR, stream=sys.stdout)
+    def __init__(self, args, config):
+        logging.basicConfig(format="%(message)s", level=logging.ERROR, stream=sys.stdout, flush=True)
         self.__data = {}
         self.__dataVM = []
         self.__hostname = platform.node().split(".", 1)[0]
-        self.__victoriaURL = f'http://{args.endpoint}:{args.port}/api/v1/import/prometheus'
+        self.__victoriaURL = f"http://{args.endpoint}:{args.port}/api/v1/import/prometheus"
+        self.__pushFrequencyMins = config["omnistat.usermode"].getint("push_frequency_mins", 5)
+        if self.__pushFrequencyMins < 1:
+            logging.error("")
+            logging.error("[ERROR]: Please set data_frequency_mins >= 1 minute (%s)" % self.__pushFrequencyMins)
+            sys.exit(1)
 
-        # Create a die file - process terminates cleanly if this file is removed
-        user = getpass.getuser()
-        self.__dieFile = ".omnistat_standalone_%s" % user
-        try:
-            with open(self.__dieFile, "w") as file:
-                file.write("Remove this file to terminate standalone data collection")
-        except:
-            logging.error("Unable to create die file -> %s" % self.__dieFile)
-
-        logging.info("Die file created (%s)" % self.__dieFile)
+        logging.info("Cached data will be pushed every %i minute(s)" % self.__pushFrequencyMins)
 
         # Init glibc for usleep access
         self.__libc = ctypes.CDLL("libc.so.6")
@@ -116,6 +118,10 @@ class Standalone:
                     continue
                 for sample in metric.samples:
                     token = self.tokenizeMetricName(sample.name, sample.labels)
+                    # account for possibility of dynamic metric definitions (e.g. annotations)
+                    if token not in self.__data:
+                        self.__data[token] = []
+                        logging.debug("Enabling caching for metric -> %s" % token)
                     self.__data[token].append([timestamp, sample.value])
                     labels = 'instance="%s"' % self.__hostname
                     if sample.labels:
@@ -123,21 +129,14 @@ class Standalone:
                             labels += ',%s="%s"' % (key, value)
                     entry = "%s{%s} %s %i" % (sample.name, labels, sample.value, int(timestamp.timestamp() * 1000))
                     self.__dataVM.append(entry)
+                    if token == "rmsjob_annotations":
+                        print("%s: %s" % (timestamp, sample.value))
 
-    def checkForTermination(self):
-        if os.path.exists(self.__dieFile):
-            return False
-        else:
-            return True
-
-    def dumpCache(self, mode="raw", filename="/tmp/omnistat_data"):
-
-        hostname = platform.node().split(".", 1)[0]
+    def dumpCache(self, mode="raw"):
 
         if mode == "raw":
             print(self.__data)
         elif mode == "victoria":
-            # victoria_url = "http://localhost:8428/api/v1/import/prometheus"
             logging.info("")
             logging.info("Pushing local node telemetry to VictoriaMetrics endpoint -> %s" % self.__victoriaURL)
             try:
@@ -148,124 +147,158 @@ class Standalone:
                 logging.error("ERROR: Collected data not saved :-(")
                 logging.error("")
                 sys.exit(4)
-
-        elif mode == "pandas-sqlite":
-            filename += ".db"
-            logging.info("Save local node telemetry in pandas/sqlite format -> %s" % filename)
-            # no hyphens for sql
-            hostname = hostname.replace("-", "_")
-            output = {}
-            if os.path.exists(filename):
-                os.remove(filename)
-
-            # Save to sql DB
-            with sqlite3.connect(filename) as conn:
-                for metric in self.__data:
-                    data = self.__data[metric]
-                    df = pd.DataFrame(data, columns=["Timestamp", "Value"])
-
-                    if metric.startswith("card"):
-                        card, delim, name = metric.partition("_")
-                        table_name = "%s__%s__%s" % (hostname, card, name)
-                    else:
-                        table_name = "%s__%s" % (hostname, metric)
-                    df.to_sql(table_name, conn, if_exists="append", index=False)
-
-        elif mode == "pandas-hdf5":
-            filename += ".h5"
-            warnings.filterwarnings("ignore", category=tables.exceptions.NaturalNameWarning)
-            output = {}
-
-            if os.path.exists(filename):
-                os.remove(filename)
-            for metric in self.__data:
-                data = self.__data[metric]
-                df = pd.DataFrame(data, columns=["Timestamp", "Value"])
-                if metric.startswith("card"):
-                    card, delim, name = metric.partition("_")
-                    hdfPath = "%s/%s/%s" % (hostname, card, name)
-                else:
-                    hdfPath = "%s/%s" % (hostname, metric)
-                df.to_hdf(filename, key=hdfPath, mode="a", format="fixed")
         else:
             logging.error("Unsupported dumpCache mode -> %s" % mode)
             sys.exit(1)
 
+    def polling(self, monitor, interval_secs):
+        """main polling function"""
 
-def main():
+        num_samples = 0
+        sample_duration = 0
+        num_pushes = 0
+        push_check_duration = 0.0
+        push_frequency_secs = self.__pushFrequencyMins * 60
+        push_time_accumulation = 0.0
+        interval_microsecs = int(interval_secs * 1000000)
+
+        # ---
+        # main sampling loop
+        try:
+            while not terminateFlagEvent.is_set():
+                start_time = time.perf_counter()
+                timestamp = datetime.now(timezone.utc)
+                monitor.updateAllMetrics()
+                self.getMetrics(timestamp)
+                num_samples += 1
+                sample_duration += time.perf_counter() - start_time
+
+                if push_check_duration > push_frequency_secs:
+                    push_check_duration = 0.0
+                    if True:
+                        try:
+                            push_start_time = time.perf_counter()
+                            self.dumpCache(mode="victoria")
+                            self.__dataVM.clear()
+                            num_pushes += 1
+                            push_time_accumulation = time.perf_counter() - push_start_time
+                        except:
+                            pass
+
+                self.sleep_microsecs(interval_microsecs)
+                # time.sleep(interval_secs)
+                push_check_duration += time.perf_counter() - start_time
+
+        except KeyboardInterrupt:
+            logging.info("")
+            logging.info("Terminating data collection from keyboard interrupt.")
+
+        # end sampling loop
+        # ---
+
+        if len(self.__dataVM) > 0:
+            try:
+                logging.info("Initiating final data push...")
+                self.dumpCache(mode="victoria")
+            except:
+                pass
+
+        logging.info("--> Sampling interval          = %.4f (secs)" % interval_secs)
+        logging.info("--> Total # of samples         = %i" % num_samples)
+        if num_samples > 0:
+            logging.info("--> Average time/sample        = %.4f (secs)" % (sample_duration / num_samples))
+        logging.info("--> Total data pushes          = %i" % num_pushes)
+        if num_pushes > 0:
+            logging.info("--> Average push duration      = %.4f (secs)" % (push_time_accumulation / num_pushes))
+        duration_secs = num_samples * interval_secs
+        if duration_secs >= 3600:
+            logging.info("--> Data collection duration   = %.4f (hours)" % (1.0 * duration_secs / 3600.0))
+        elif duration_secs >= 60:
+            logging.info("--> Data collection duration   = %.4f (mins)" % (1.0 * duration_secs / 60.0))
+        else:
+            logging.info("--> Data collection duration   = %.4f (secs)" % duration_secs)
+
+        # deliver event to shutdown procedure
+        logging.debug("setting shutdown delivery event")
+        dataDeliveredEvent.set()
+        logging.debug("shutdown delivery event is set")
+        time.sleep(0.5)
+
+        logging.info("Terminating execution...")
+        os.kill(os.getpid(), signal.SIGTERM)
+        return
+
+
+def parse_args():
+    """Parse command-line arguments
+
+    Returns:
+        argparse.Namespace: parsed arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--configfile", type=str, help="runtime config file", default=None)
-    parser.add_argument("--interval", type=float, help="sampling frequencey (in secs)", default=0.5)
+    parser.add_argument("--interval", type=float, help="sampling frequency (in secs)", default=0.5)
     parser.add_argument("--logfile", type=str, help="redirect stdout to logfile", default=None)
-    parser.add_argument("--endpoint",type=str,help="hostname of VictoriaMetrics server", default="localhost")
-    parser.add_argument("--port",type=int,help="port to access VictoriaMetrics server", default=8428)
-    args = parser.parse_args()
+    parser.add_argument("--endpoint", type=str, help="hostname of VictoriaMetrics server", default="localhost")
+    parser.add_argument("--port", type=int, help="port to access VictoriaMetrics server", default=8428)
 
-    interval_secs = args.interval
-    assert interval_secs > 0.005
+    return parser.parse_args()
 
+
+@app.route("/shutdown")
+def terminate():
+    """Endpoint that can be used to terminate execution"""
+    logging.info("Received shutdown request")
+    terminateFlagEvent.set()
+
+    # spin loop till notice recieved that last data was pushed
+    maxChecks = 0
+    while not dataDeliveredEvent.isSet():
+        logging.debug("waiting for data delivery event...")
+        maxChecks += 1
+        if maxChecks > 100:
+            break
+        time.sleep(0.1)
+
+    return jsonify({"message": "Shutting down..."}), 200
+
+
+@app.route("/metrics")
+def heartbeat():
+    """Endpoint that can be used to confirm exporter is running"""
+    return jsonify({"status": "ok"}), 200
+
+
+def runFlask(config):
+    listenPort = config["omnistat.collectors"].get("port", 8001)
+    app.run(host="0.0.0.0", port=listenPort)
+
+
+def main():
+
+    args = parse_args()
     config = utils.readConfig(utils.findConfigFile(args.configfile))
+    print(config)
 
     # Initialize GPU monitoring
     monitor = Monitor(config, logFile=args.logfile)
     monitor.initMetrics()
+
     # Initialize standalone polling
-    caching = Standalone(args)
-    caching.initMetrics(prefix=("rocm", "rmsjob_"))
+    if args.interval < 0.005:
+        logging.error("")
+        logging.error("[ERROR]: Please set sampling interval to be >= 5 millisecs (%s)" % args.interval)
+        exit(1)
 
-    interval_microsecs = int(interval_secs * 1000000)
-    exit_check_interval_secs = 5.0
+    caching = Standalone(args, config)
+    caching.initMetrics()
 
-    # ---
-    # Main polling loop
-    num_samples = 0
-    sample_duration = 0
-    exit_check_duration = 0
-    mem_mb_base = utils.getMemoryUsageMB()
+    # Launch flask app as thread so we can respond to remote shutdown requests
+    flask_thread = threading.Thread(target=runFlask, args=[config])
+    flask_thread.start()
 
-    try:
-        while True:
-            start_time = time.perf_counter()
-            timestamp = pd.Timestamp("now")
-            monitor.updateAllMetrics()
-            caching.getMetrics(timestamp, prefix=("rocm", "rmsjob_"))
-            sample_duration += time.perf_counter() - start_time
-
-            num_samples += 1
-
-            if exit_check_duration > exit_check_interval_secs:
-                logging.debug("Check if received request to terminate")
-                exit_check_duration = 0.0
-                if caching.checkForTermination():
-                    logging.info("Terminating per request...")
-
-                    break
-
-            caching.sleep_microsecs(interval_microsecs)
-            exit_check_duration += time.perf_counter() - start_time
-    except KeyboardInterrupt:
-        logging.info("")
-        logging.info("Terminating data collection from keyboard interrupt.")
-
-    # end polling loop
-    # ---
-
-    logging.info("--> Sampling interval          = %f (secs)" % interval_secs)
-    logging.info("--> Total # of samples         = %i" % num_samples)
-    logging.info("--> Average time/sample        = %.5f (secs)" % (sample_duration / num_samples))
-
-    mem_mb = utils.getMemoryUsageMB()
-    logging.info("--> Base memory in use         = %.3f MB" % mem_mb_base)
-    logging.info("--> Memory growth from caching = %.3f MB" % (mem_mb - mem_mb_base))
-
-    duration_secs = num_samples * interval_secs
-    mem_per_hour = 3600.0 * ((mem_mb - mem_mb_base) / duration_secs)
-    logging.info("--> Data collection duration   = %.2f (secs)" % duration_secs)
-    logging.info("--> Approx. mem growth/hour    = %.3f (MB)" % mem_per_hour)
-
-    hostname = platform.node().split(".", 1)[0]
-    # caching.dumpCache(mode="pandas-sqlite", filename="/tmp/omnistat.%s" % hostname)
-    caching.dumpCache(mode="victoria", filename="/tmp/omnistat.%s" % hostname)
+    # Initiate main polling loop/data collection
+    caching.polling(monitor, args.interval)
 
 
 if __name__ == "__main__":
