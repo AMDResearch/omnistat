88feb7b02e0dc4d8e6a5e2a24b674c4f40d14f78
Karl W. Schulz <karl.schulz@amd.com>
2025-02-11 15:58:46 -0600

    include license header
    Signed-off-by: Karl W. Schulz <karl.schulz@amd.com>

--- docker/index.py
+++ docker/index.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
+# -------------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2023 - 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -------------------------------------------------------------------------------
 #
 # omnistat-index -- Scan Omnistat database and serve list of jobs
 #
 # Scan an Omnistat database available at a given URL with bounded accuracy.
 # Using a single PromQL query over very long time ranges (>=1m) requires a very
-# coarse sampling granularity, leading to unaccurate results in Grafana panels.
-# This script generates many PromQL queries over short time ranges (~1d),
+# coarse sampling granularity, leading to potentially inaccurate results in Grafana
+# panels. This script generates many PromQL queries over short time ranges (~1d),
 # allowing much more fine-grained sampling.
 #
 # This approach is helpful to guarantee highly accurate job information and
